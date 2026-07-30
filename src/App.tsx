7ab3e5b3b9d4e2a4c6d675a8afe7ec42706a78cd
rayaanr <rayaannsn@gmail.com>
2026-07-30 12:47:19 +0530

    feat: Add project page back

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import AboutPage from "./pages/AboutPage";
-// import ProjectsPage from "./pages/ProjectsPage";
+import ProjectsPage from "./pages/ProjectsPage";
 import ShelfPage from "./pages/ShelfPage";
 import WritingPage from "./pages/WritingPage";
 import ArticlePage from "./pages/ArticlePage";
@@ -27,7 +27,7 @@ const App = () => {
         <main className="max-w-6xl mx-auto px-6 pt-36 pb-24 min-h-screen">
           <Routes>
             <Route path="/" element={<AboutPage />} />
-            {/* <Route path="/projects" element={<ProjectsPage />} /> */}
+            <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/shelf" element={<ShelfPage />} />
             <Route path="/writing" element={<WritingPage />} />
             <Route path="/writing/:id" element={<ArticlePage />} />
